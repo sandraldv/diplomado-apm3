f10a90af98921853f38dac2b68ec0b782fa9f008
sandraldv <sandra_duranv@hotmail.com>
2015-10-10 13:00:17 -0500

    10102015

---
+++ modulo 3/Lab 7/js/lab7.js
@@ -0,0 +1,29 @@
+
+var a,b,c,d;
+
+// Utilizando el objeto Date de JS para trabajar con fechas, tiempo y timestamps
+
+my_object={
+	a:5,
+	b:9,
+	c:8,
+	d:1,
+}
+
+
+if(my_object.a > my_object.b){
+	 
+     console.log(my_object.a+my_object.b)
+} else {
+	
+		if(my_object.a < my_object.b){
+
+		console.log(my_object.a*my_object.b)
+} 	else {
+
+			if(my_object.a === my_object.b){
+
+		console.log(my_object.a/my_object.b)
+		}
+	}
+}
